867faa85608af889e787ed4aa909e93f6f912eca
smartvain <zlc.blue15210@gmail.com>
2023-09-21 00:35:07 +0900

    add store

---
+++ app/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit"
+import cameraReducer from '@/features/camera'
+
+const store = configureStore({
+  reducer: cameraReducer,
+  middleware: getDefaultMiddleware => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['camera/setPosition'],
+        ignoredPaths: ['position']
+      }
+    })
+})
+
+export default store
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
