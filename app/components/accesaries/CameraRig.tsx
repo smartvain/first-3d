88734aa57f1ca19cfbe996bca31dc68174362b3f
ryuichi-amejima-coconala <ryuichi.amejina@coconala.com>
2023-10-15 15:45:15 +0900

    adjust lookAt

--- app/components/accesaries/CameraRig.tsx
+++ app/components/accesaries/CameraRig.tsx
@@ -6,6 +6,6 @@ export default function CameraRig({ vec3 = new THREE.Vector3() }) {
   return useFrame((state) => {
     const time = state.clock.elapsedTime
     state.camera.position.lerp(vec3.set(4 + Math.sin(time / 5), 0, 2 + Math.cos(time / 5) / 2), 0.05)
-    state.camera.lookAt(0, 0, 0)
+    state.camera.lookAt(0, -0.5, 0)
   })
 }
